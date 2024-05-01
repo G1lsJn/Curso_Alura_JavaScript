b0029797a67818a347961de0552979cc26763625
Gilson Jr <gilsondiasjn15@gmail.com>
2024-05-01 15:49:13 -0300

    finalizando o site de toca som

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,6 +1,43 @@
 
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    // Tratamento de erros
+    if (elemento  != null && elemento.localName === 'audio'){
+        elemento.play();
+    } else{
+        console.log("O seletor não foi encontrado ou inválido!");
+        }
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+//seleciona todas as teclas do jogo
+const listaDeTeclas = document.querySelectorAll(".tecla"); 
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++){
+
+    //captura a tecla atual
+    const tecla = listaDeTeclas[contador];
+
+    //pega o nome da classe que é o instrumento
+    const instrumento = tecla.classList[1];
+
+    // Templete string
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    // Configurando as teclas do teclado pra manipular o site
+    tecla.onkeydown = function (evento){
+        if (evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('ativa');
+        }
+        
+    }
+
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa')
+    }
+
+}
